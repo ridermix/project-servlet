6411cdc1b288bc99fc7d93e3ee1bdc2f9bdb6bd4
rustekapl <rustekapl@gmail.com>
2023-02-15 15:09:58 +0200

    Implemented Draw

--- src/main/java/com/tictactoe/LogicServlet.java
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -34,6 +34,12 @@ public class LogicServlet extends javax.servlet.http.HttpServlet{
             if(checkWin(resp, currentSession, field)){
                 return;
             }
+        } else {
+            currentSession.setAttribute("draw", true);
+            List<Sign> data = field.getFieldData();
+            currentSession.setAttribute("data", data);
+            resp.sendRedirect("/index.jsp");
+            return;
         }
         List<Sign> data = field.getFieldData();
 

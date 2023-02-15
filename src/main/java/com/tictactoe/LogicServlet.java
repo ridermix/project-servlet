42693c3e7618e8ece15042057bbe367ddf3092a7
rustekapl <rustekapl@gmail.com>
2023-02-15 11:11:44 +0200

    Added LogicServlet

---
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -0,0 +1,22 @@
+package com.tictactoe;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LogicServlet", value = "/logic")
+public class LogicServlet extends javax.servlet.http.HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int index = getSelectedIndex(req);
+        resp.sendRedirect("/index.jsp");
+    }
+
+    protected int getSelectedIndex(HttpServletRequest request){
+        String click = request.getParameter("click");
+        boolean isNumeric = click.chars().allMatch(Character::isDigit);
+        return isNumeric ? Integer.parseInt(click) : 0;
+    }
+}
